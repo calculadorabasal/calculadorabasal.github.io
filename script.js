1f5dcac533811cedc362d11eb9baf9537b09cba7
calculadorabasal <144759789+calculadorabasal@users.noreply.github.com>
2023-09-14 11:41:29 -0400

    unit commit

--- script.js
+++ script.js
@@ -5,21 +5,32 @@ const INPUT = document.getElementById("peso");
 const BOTON = document.getElementById("calcular");
 
 BOTON.addEventListener("click", () => {
-    let peso = INPUT.value;
+    let peso = INPUT.valueAsNumber;
 
-    if(peso > 0){
+    if(peso > 0 && peso <= 30){
         ERROR.style.display = "none";
+
        let res = holliday(peso);
-       let man = peso*1.5;
-       FLU.innerHTML = res + " cc";
-       MAN.innerHTML = "m+m/2";
+       let man = (res/24)*1.5;
+       FLU.innerHTML = (res/24) + " cc/h";
+       MAN.innerHTML = "m+m/2 " + man + " cc/h";
        FLU.style.display = "block";
        MAN.style.display = "block";
-    } else{
-        ERROR.style.display = "block";
-        FLU.style.display = "none"
-        MAN.style.display = "none"
+    } else if (peso>30){
+        ERROR.style.display = "none";
 
+        let respuesta = superficieCorporal(peso);
+        let mante = (respuesta * 2000);
+        FLU.innerHTML = respuesta * 1500 + " diario" + " " + (respuesta*1500)/24 + " cc/h";
+        FLU.style.display = "block"
+        MAN.innerHTML = mante + " diario" + mante/24 + " cc/h";
+        MAN.style.display = "block";
+
+    } else {
+        console.log ("error");
+        ERROR.style.display = "block";
+        FLU.style.display = "none";
+        MAN.style.display = "none";
     }
 });
 
@@ -34,6 +45,14 @@ function holliday(peso){
         volumen = 1500 + (peso-20)*20;
     }
     return volumen;
+}
+
+function superficieCorporal(peso){
+    let flujo;
+    if (peso> 30){
+        flujo = ((peso*4)+7)/(peso+90)
+    }
+    return flujo;
 }
 
 
